a8914e74c0d52bf6356b49b41d212e8a2b0dadca
chourasiya <vivekchourasiya95@gmail.com>
2018-07-20 02:59:16 -0700

    next commit

--- server.js
+++ server.js
@@ -1,50 +1,39 @@
 const express = require('express');
 const hbs = require('hbs');
-
-const port = process.env.PORT || 3000;
+const fs = require('fs');
 var app = express();
-
-hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine', 'hbs');
-
-app.use((req, res, next) => {
-  var now = new Date().toString();
-  console.log(req.url)
-  next();
-})
+app.use(express.static(__dirname+'/public'));
 
 // app.use((req, res, next) => {
 //   res.render('maintainance.hbs');
 // });
-
-app.use(express.static(__dirname + '/public'));
-
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 });
-
 hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
-
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMeassage:'Welcome to my website',
+    welcomeMeassage: 'Welcome to my website',
   });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about.hbs', {
-      pageTitle: 'About Page',
-    });
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
 });
 
 app.get('/bad', (req, res) => {
-    res.send({
-      errorMessage: "Unable to handle request"
-    });
+  res.send({
+    error: 'Unable to handle'
+  });
 });
+
 app.listen(3000, () => {
-  console.log('Server is up on port'+port);
+  console.log('server is up on port 3000');
 });
